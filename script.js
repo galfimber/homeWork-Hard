bb3cae0550ca5023704549a792cb267879cb9f43
Nikita Varankin <galfimber2014@yandex.ru>
2023-11-14 22:01:00 +0500

    Выполнено усложненное задание 5

--- script.js
+++ script.js
@@ -1,53 +1,29 @@
-const lang = prompt("Выберите язык: ru или en");
-if (lang == "ru") {
-  console.log(
-    "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье"
-  );
-} else if (lang == "en") {
-  console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
-} else {
-  console.log("Выбран неверный язык");
-}
+let arr = ["2", "10", "12", "20", "34", "40", "42"];
 
-switch (lang) {
-  case "ru":
-    console.log(
-      "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье"
-    );
-    break;
-  case "en":
-    console.log(
-      "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"
-    );
-    break;
-  default:
-    console.log("Выбран неверный язык");
-}
-
-const week = {
-  ru: [
-    "Понедельник",
-    "Вторник",
-    "Среда",
-    "Четверг",
-    "Пятница",
-    "Суббота",
-    "Воскресенье",
-  ],
-  en: [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ],
+const isSimple = function (num) {
+  let i = 2;
+  const limit = Math.sqrt(num);
+  while (i <= limit) {
+    if (num % i === 0) {
+      return false;
+    }
+    i += 1;
+  }
+  return true;
 };
-console.log(week[lang]);
 
+console.log("Задание 1")
 
-const namePerson = prompt("Введите имя");
-namePerson === 'Артем' ? console.log('директор') 
-              : namePerson == 'Александр' ? console.log('преподаватель') 
-              : console.log('студент');
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i].at(0) === "2" || arr[i].at(0) === "4") {
+    console.log(arr[i]);
+  }
+}
+
+console.log("Задание 2")
+
+for (let i = 2; i <= 100; i++) {
+  if (isSimple(i)) {
+    console.log(`Делители этого числа: 1 и ${i}`);
+  }
+}
